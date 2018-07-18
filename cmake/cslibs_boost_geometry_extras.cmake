516e325088e8f8bbc0b7d89d25831f3e4bf10ca8
Richard Hanten <richard.hanten@uni-tuebingen.de>
2018-07-18 17:02:40 +0200

    flags

--- cmake/cslibs_boost_geometry_extras.cmake
+++ cmake/cslibs_boost_geometry_extras.cmake
@@ -1,1 +1,14 @@
-add_definitions("-Ofast -march=native -ffast-math")
+add_definitions(-W
+                -Wall
+                -Wno-unused-parameter
+                -Wno-unused-function
+                -fno-strict-aliasing
+                -Wno-deprecated-register
+                -march=native)
+
+if(NOT ${CMAKE_BUILD_TYPE} STREQUAL Debug)
+    add_definitions(-Ofast
+                     -rdynamic
+                     -ffast-math)
+    message("[${PROJECT_NAME}]: Compiling with optimization!")
+endif()
